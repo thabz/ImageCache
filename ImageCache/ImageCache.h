4bf89ecd9ba16facf515046b08ff1a0a1535eafe
Jesper Christensen <jesper@kalliope.org>
2015-09-28 13:07:33 +0200

    Initial Commit

---
+++ ImageCache/ImageCache.h
@@ -0,0 +1,19 @@
+//
+//  ImageCache.h
+//  ImageCache
+//
+//  Created by Jesper Christensen on 28/09/2015.
+//  Copyright © 2015 Jesper Christensen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCache.
+FOUNDATION_EXPORT double ImageCacheVersionNumber;
+
+//! Project version string for ImageCache.
+FOUNDATION_EXPORT const unsigned char ImageCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCache/PublicHeader.h>
+
+
